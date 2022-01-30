317045714c61f970f302823250880142e3f93638
Karina <rbthio@gmail.com>
2022-01-30 13:10:27 +0200

    Tests

---
+++ src/main/test/base/GameTest.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Testing test");
+    }
+}
