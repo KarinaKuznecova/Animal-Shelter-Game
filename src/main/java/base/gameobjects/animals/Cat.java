4a129140a8d8de3a7fdd68ee843a97598351a8ec
Karina <rbthio@gmail.com>
2022-01-30 14:21:05 +0200

    Recolored rats

--- src/main/java/base/gameobjects/animals/Cat.java
+++ src/main/java/base/gameobjects/animals/Cat.java
@@ -2,16 +2,11 @@ package base.gameobjects.animals;
 
 import base.gameobjects.Animal;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static base.constants.Constants.*;
+import static base.constants.Constants.CAT_BLACK;
 
 public class Cat extends Animal {
 
     public static final String NAME = "cat";
-
-    public static final List<String> colors = Arrays.asList(CAT_WHITE, CAT_BROWN, CAT_CARAMEL, CAT_BLACK);
 
     public Cat(int startX, int startY, int speed, String color) {
         super(color != null ? NAME + "-" + color : CAT_BLACK, startX, startY, speed, 32, MAX_HUNGER, MAX_THIRST);
